07f8eeed80e93c391a36b9aaa90507047927f104
sriman <srimanbachu26@gmail.com>
2026-03-29 23:19:37 +0530

    Initialize frontend project with React, TypeScript, and Vite setup. Added essential files including package.json, ESLint configuration, and basic HTML structure. Included styles and assets for the opening page, along with a README for project guidance.

---
+++ frontend/src/OpeningPage.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import flair from "./assets/flair.png";
+import video from './assets/background.mp4'
+import './OpeningPage.css'
+
+const OpeningPage = () => {
+  return (
+    <div className="main">
+
+      <video className="backgroundVideo" autoPlay loop muted playsInline>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="fake">
+          <div className="child">
+            <h1 className='flairText'>
+              Flair  <img src={flair} alt="fuckyou" className="flairImg" />
+            </h1>
+            <p>Create. Style. Download. Repeat
+            </p>
+            <br />
+            
+            <button className='startButton'>Get Started </button>
+            
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default OpeningPage  
