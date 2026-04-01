3fd30b9cb84040609d73ca292e13bd0ec56fe3ba
sriman <srimanbachu26@gmail.com>
2026-04-02 02:24:36 +0530

    had made ui of my main page

--- frontend/src/CodeComponent/Dcode.tsx
+++ frontend/src/CodeComponent/Dcode.tsx
@@ -1,10 +1,19 @@
 import './Dcode.css'
+import flair from '../assets/flair.png'
 
 const Dcode = () => {
   return (
     <div className='main'>
       <div className="nav">
+      <div className="switch">
 
+      </div>
+          <div className="logo">
+          <h1>Flair</h1> <img src={flair} alt="flair_img"  className='flairimg'/>
+          </div>
+       <div className="empty">
+        
+        </div>   
       </div>
       <div className="bottom">
         <div className="sidebar">
