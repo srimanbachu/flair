89d8d29a253fbaf3509d74c623553af0dc0ebc8b
sriman <srimanbachu26@gmail.com>
2026-04-02 18:22:43 +0530

    i had made changes in my main code card editor

--- frontend/src/CodeComponent/Dcode.tsx
+++ frontend/src/CodeComponent/Dcode.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import './Dcode.css'
 import flair from '../assets/flair.png'
+import { Menu } from 'lucide-react'
 
 const Dcode = () => {
+
+  const [show, setShow] = useState()
+
   return (
     <div className='main'>
       <div className="nav">
+      <div className="menudiv">
+      <Menu  className='menuicon'/>
       <div className="switch">
-
+        <div className="imagecard">imagecard</div>
+        <div className="codecard">codecard</div>
+      </div>
       </div>
           <div className="logo">
           <h1>Flair</h1> <img src={flair} alt="flair_img"  className='flairimg'/>
